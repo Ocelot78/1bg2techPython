8544e80b491edff88e9f62e1ca824d942cc24368
1B _9_WK <114579256+Ocelot78@users.noreply.github.com>
2022-10-03 11:19:33 +0000

    zadania 1 2 3 4 zadanie 5 w toku

---
+++ KartaPracy1.py
@@ -0,0 +1,30 @@
+#zad 1
+
+
+#a = int(input())
+#b = int(input())
+#print("obliczanie")
+#print(a**2 + b**2)
+
+#zad 2
+
+#a = int(input())
+#b = int(input())
+#print((a+b)**2)
+
+#zad 3
+#a = int(input())
+#b = int(input())
+#print((a-b)**3)
+
+#zad 4
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#print(a*b*c)
+
+#zad 5
+a, b = map(int,(input().split())
+print(2*(a+b)/5)
+print(a+b)
+           
