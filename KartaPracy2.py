292c6fc51501cb5620aa7276c2544a9f3fd0197b
1B _9_WK <114579256+Ocelot78@users.noreply.github.com>
2022-10-05 11:19:47 +0000

    cos

---
+++ KartaPracy2.py
@@ -0,0 +1,25 @@
+#Zad 1
+# a = int(input("Podaj liczbe dzbanie:"))
+# if a % 3 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+#zad 2
+# a = int(input("Podaj liczbe śmieciu: "))
+# if a>=100 and a<1000 and a%17==0:
+#   print("TAK")
+# else:
+#   print("NIE")
+#Zad 3
+# wiek = int(input("Witamy na policji podaj swój wiek bo tak: "))
+# if wiek>=18:
+#   print("jesteś osobą pełnoletnią jesteś poszukiwany w 30 krajach i w całej unii europejskiej")
+# else:
+#   print("jesteś nie pełnoletni uciekaj z polski bo policja cie szuka")
+#Zad 4
+LIMIT = 20
+waga = int(input("Ile waży ten samochód"))
+if waga <=LIMIT:
+  print("jedz sobie")
+else:
+  print("stop")
