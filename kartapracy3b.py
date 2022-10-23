d2699cb53defd75dd9b894f9c43eff8a6dea7a81
1B _9_WK <114579256+Ocelot78@users.noreply.github.com>
2022-10-23 16:46:08 +0000

    kartapracy 3b zadanie 5

---
+++ kartapracy3b.py
@@ -0,0 +1,28 @@
+#Zad 1 
+# for i in range(1,31):
+#   print(i, end=" ")
+#Zad 2
+# for i in range(1,10):
+#   print(i**2, end=" ")
+#Zad 3
+# for i in range(1137, 10000, 379):
+#   print(i, end=" ")
+#Zad 4
+# for i in range(100,1000,5):
+#   print(i, end=" ")
+# for i in range(102,1000,6):
+#   print(i, end=" ")
+# for i in range(110,1000,11):
+#   print(i, end=" ")
+#Zad 5
+# nliczb = int(input())
+# suma = 0
+# for i in range(0, nliczb):
+#   n = int(input())
+#   suma = suma + n
+# print(suma)
+#Zad 6
+
+
+
+
