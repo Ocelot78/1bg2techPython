3055b7476593054ffca12c2c9557301305f0b71d
1B _9_WK <114579256+Ocelot78@users.noreply.github.com>
2022-11-02 12:20:18 +0000

    213

---
+++ PrzedSpr1.py
@@ -0,0 +1,25 @@
+# Zad 1 - Medium - Oblicz sumę k początkowych liczb  trzycyfrowych
+# k = int(input())
+# suma = 0
+# for i in range(100,100 + k):
+#   suma = suma + i
+# print(suma)
+# Zad 2 - Hard - Oblicz sumę n początkowych wyrazów ciągu fibonacciego
+# n = int(input())
+# a, b = 0,1
+# for i in range(n):
+#   a, b = b, a + b
+#   print(a, end=" ")
+# Zad 3 - Hard - Spradź czy liczba wpisana przez usera jest doskonała
+n = int(input())
+for i in range(1, n+1):
+  if n % i == 0:
+    
+
+
+
+
+# Zad 4 - Medium - Oblicz sumę liczb dwucyfrowych parzystych podzielnych przez 7
+# for i in range(14,100,7):
+#   print(i, end=" ")
+    
--- kartapracy3a.py
+++ kartapracy3a.py
@@ -13,7 +13,9 @@
 #Zad 3
 n = int(input())
 for i in range(1,n+1):
-  print("*"*i)
-if
-  
+  print("*", end=" ")
+  if i % 2 == 0:
+    print("-"*i, end=" ")
+  else:
+    print("|"*i, end=" ")
   
