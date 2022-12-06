49b5850c91a7a22cb67fe9f63014cc12047f203c
1B _9_WK <114579256+Ocelot78@users.noreply.github.com>
2022-12-06 16:53:16 +0000

    test 2a

--- KartyPracy/kartapracy2a.py
+++ KartyPracy/kartapracy2a.py
@@ -37,7 +37,7 @@
 # d = int(input())
 # print(min(a,b,c,d))
 
-#zad 5
+# zad 5
 # a = int(input())
 # b = int(input())
 # c = int(input())
@@ -63,3 +63,37 @@
 #   print("Trójką Rozwatokątny")
 # elif a**2 + b**2 > c**2:
 #   print("Trójkąt ostrokątny")
+#TEST
+#zad 1 
+# a, b = int(input()), int(input())
+# if (a+b)% 2 == 0:
+#   print("tak")
+# else:
+#   print("Nie")
+#zad 2 
+# import math
+# a, g = int(input()), int(input())
+# if (a+g)/2 > math.sqrt(a * g):
+#   print("Tak")
+# else:
+#   print("Nie")
+#Zad 3 
+# k, l , m = int(input()), int(input()), int(input())
+# if k == l:
+#   print(k,"=",l)
+# elif k == m:
+#   print(k,"=",m)
+# elif l == m:
+#   print(l,"=",m)
+# else:
+#   print("NIE")
+#Zad 4
+# a,b,c,d = int(input()),int(input()),int(input()),int(input())
+# print(min(a,b,c,d))
+#Zad 5
+# a,b,c = int(input()),int(input()),int(input())
+# if a+b>c and b+c>a and a+c>b:
+#   print("Tak")
+# else:
+#   print("Nie")
+#Zad 6
---
+++ cezar/pre.py
@@ -0,0 +1,22 @@
+# print(ord("A"))
+# print(ord("B"))
+# print(ord("Z"))
+
+# (chr(65))
+# print(chr(75))
+# (chr(89))
+
+# a = 1
+# for i in range(65,91):
+#   print(chr(a + 64))
+#   a += 1
+
+napis = "POLSKA"
+# print(len(napis))
+# print(napis[0])
+# print(napis[3])
+# print(napis[5])
+# print(napis[4])
+
+for i in range(len(napis)):
+  print(ord(napis[i]))
